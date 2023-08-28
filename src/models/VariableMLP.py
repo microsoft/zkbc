b5b46315e3ad1506550a544fb7158b69801eb065
tobinsouth <tobin.south@gmail.com>
2023-08-28 20:17:11 +0000

    Making MLP for editable

--- src/models/VariableMLP.py
+++ src/models/VariableMLP.py
@@ -2,14 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class MLP(nn.Module):
-    def __init__(self, nlayer=1):
-        super(MLP, self).__init__()
-        self.input_layer = nn.Linear(256, 512)    
+class VariableMLP(nn.Module):
+    def __init__(self, nlayer=1, hidden_size=256, input_size=256, output_size=1):
+        super(VariableMLP, self).__init__()
+        self.input_layer = nn.Linear(input_size, hidden_size)    
         self.hidden_layers = nn.ModuleList()
         for _ in range(nlayer):
-            self.hidden_layers.append(nn.Linear(512, 512))
-        self.output_layer = nn.Linear(512, 1) 
+            self.hidden_layers.append(nn.Linear(hidden_size, hidden_size))
+        self.output_layer = nn.Linear(hidden_size, output_size) 
 
     def forward(self, x):
         x = torch.relu(self.input_layer(x))
