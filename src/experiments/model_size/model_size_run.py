e527f05301c2222cdf82d323f614d703e6b917a9
tobinsouth <tobin.south@gmail.com>
2023-12-03 18:16:02 -0500

    New experiments

--- src/experiments/model_size/model_size_run.py
+++ src/experiments/model_size/model_size_run.py
@@ -4,11 +4,12 @@ from tqdm import tqdm
 import numpy as np
 import os, sys
 import time
-from concurrent.futures import ThreadPoolExecutor, as_completed
 import pickle, json
 from thop import profile
 import timeit
+import ezkl
 
+# Import the existing models from src/models
 sys.path.append('../..')
 from utils.export import export
 from models.VariableCNN import VariableCNN
@@ -16,12 +17,10 @@ from models.VariableMLP import VariableMLP
 from models.VariableLSTM import VariableLSTM
 from models.SimpleTransformer import SimpleTransformer
 
-DEBUG = True
-AVAILABLE_LOGROWS = [16,17,18, 19, 21, 25]
-LOGROWS_PATH = lambda lr: f'../../../../kzgs/kzg{lr}.srs' # You may need to generate this
-NEAREST_LOGROWS_ABOVE = lambda lr: LOGROWS_PATH(min([x for x in AVAILABLE_LOGROWS if x >= lr]))
-LOGGING = True
-pipstd = lambda fname: f" 2>&1 | tee logs/{fname}.log" if LOGGING else ""
+FILENAME ='model_size_results_Dec3rd.csv'
+LOGROWS_PATH = '../../../kzgs/kzg%d.srs' # You may need to generate this
+LOGGING = False
+pipstd = lambda fname: f" >> logs/{fname}.log" if LOGGING else ""
 os.makedirs('logs', exist_ok=True)
 os.makedirs('runfiles', exist_ok=True)
 
@@ -31,17 +30,18 @@ def setup_and_prove(modeltype, nlayer):
         input_shape= [1,28,28]
         dummy_input = torch.randn(input_shape)
     elif modeltype == 'MLP':
-        model = VariableMLP(nlayer+int((nlayer**2)/2))
+        model = VariableMLP(3*nlayer, hidden_size=256+32*nlayer)
         input_shape= [1,256]
         dummy_input = torch.randn(input_shape)
     elif modeltype == 'Attn':
-        model = SimpleTransformer(int(np.sqrt(nlayer)/2)+1, d_model=64+32*nlayer)
-        input_shape= [1,16,64+32*nlayer]
+        model = SimpleTransformer(int(np.sqrt(nlayer)/2)+1, d_model= (32*nlayer if nlayer < 16 else 32*(nlayer-4)))
+        input_shape= [1,16,(32*nlayer if nlayer < 16 else 32*(nlayer-4))]
         dummy_input = torch.randn(input_shape)
     elif modeltype == 'LSTM':
         temp_nlayer = int(np.sqrt(nlayer)/2)
-        model = VariableLSTM(nlayer=temp_nlayer, input_size=8+8*nlayer, hidden_size=8+8*temp_nlayer)
-        input_shape= [3+nlayer,8+8*nlayer]
+        temp_extra = (nlayer if nlayer < 16 else nlayer - 4)
+        model = VariableLSTM(nlayer=temp_nlayer, input_size=8+8*temp_extra, hidden_size=8+8*temp_nlayer)
+        input_shape= [3+nlayer,8+8*temp_extra]
         dummy_input = torch.randn(input_shape)
     else:
         raise ValueError("modeltype must be one of CNN, MLP, Attn, LSTM")
@@ -50,25 +50,25 @@ def setup_and_prove(modeltype, nlayer):
     export(model, input_shape= input_shape, onnx_filename=f'runfiles/{modeltype+str(nlayer)}.onnx', input_filename=f'runfiles/input{modeltype+str(nlayer)}.json')
 
     logs_file = pipstd(f'{modeltype}_prework_{nlayer}')
+
     os.system(f"ezkl table -M runfiles/{modeltype+str(nlayer)}.onnx" + logs_file)
-    os.system(f"ezkl gen-settings -M runfiles/{modeltype+str(nlayer)}.onnx --settings-path=settings.json --input-visibility='public'"+logs_file )
+    os.system(f"ezkl gen-settings -M runfiles/{modeltype+str(nlayer)}.onnx --settings-path=settings.json"+logs_file)
     os.system(f"ezkl calibrate-settings -M runfiles/{modeltype+str(nlayer)}.onnx -D runfiles/input{modeltype+str(nlayer)}.json --settings-path=settings.json --target=resources"+logs_file)
-    os.system(f"ezkl compile-model -M runfiles/{modeltype+str(nlayer)}.onnx -S settings.json --compiled-model runfiles/{modeltype+str(nlayer)}.ezkl"+logs_file)
-    os.system(f"ezkl gen-witness -M runfiles/{modeltype+str(nlayer)}.ezkl -D runfiles/input{modeltype+str(nlayer)}.json --output runfiles/witness_{modeltype+str(nlayer)}.json --settings-path settings.json"+logs_file)
-    os.system(f"ezkl mock -M runfiles/{modeltype+str(nlayer)}.ezkl --witness runfiles/witness_{modeltype+str(nlayer)}.json --settings-path settings.json" + logs_file) 
+    os.system(f"ezkl compile-circuit -M runfiles/{modeltype+str(nlayer)}.onnx -S settings.json --compiled-circuit runfiles/{modeltype+str(nlayer)}.ezkl"+logs_file)
+    os.system(f"ezkl gen-witness -M runfiles/{modeltype+str(nlayer)}.ezkl -D runfiles/input{modeltype+str(nlayer)}.json --output runfiles/witness_{modeltype+str(nlayer)}.json"+logs_file)
+    os.system(f"ezkl mock -M runfiles/{modeltype+str(nlayer)}.ezkl --witness runfiles/witness_{modeltype+str(nlayer)}.json" + logs_file) 
     os.system(f"cat settings.json >> logs/{modeltype}_prework_{nlayer}.log")
 
     # Read in settings to determin SRS file size
     with open('settings.json', 'r') as f:
-        settings = json.load(f)
-        lr = settings['run_args']['logrows']
-        srs_path  = NEAREST_LOGROWS_ABOVE(lr)
+        logrows = json.load(f)['run_args']['logrows']
+    ezkl.get_srs(LOGROWS_PATH % logrows, 'settings.json')
 
-    time_to_setup = timeit.timeit(lambda: os.system(f"ezkl setup -M runfiles/{modeltype+str(nlayer)}.ezkl --srs-path={srs_path} --vk-path=vk.key --pk-path=pk.key --settings-path=settings.json" + pipstd(f'{modeltype}_setup_{nlayer}')),  number=1)
+    time_to_setup = timeit.timeit(lambda: os.system(f"ezkl setup -M runfiles/{modeltype+str(nlayer)}.ezkl --srs-path={LOGROWS_PATH % logrows} --vk-path=vk.key --pk-path=pk.key" + pipstd(f'{modeltype}_setup_{nlayer}')),  number=1)
 
     os.makedirs('proofs', exist_ok=True)
     proof_file = f"proofs/{modeltype}_proof_{nlayer}.proof"
-    time_to_prove = timeit.timeit(lambda: os.system(f"ezkl prove -M runfiles/{modeltype+str(nlayer)}.ezkl --srs-path={srs_path} --witness runfiles/witness_{modeltype+str(nlayer)}.json --pk-path=pk.key --settings-path=settings.json --proof-path={proof_file} --strategy='accum'"+ pipstd(f'{modeltype}_prove_{nlayer}')), number=1)
+    time_to_prove = timeit.timeit(lambda: os.system(f"ezkl prove -M runfiles/{modeltype+str(nlayer)}.ezkl --srs-path={LOGROWS_PATH % logrows} --witness runfiles/witness_{modeltype+str(nlayer)}.json --pk-path=pk.key --proof-path={proof_file}"+ pipstd(f'{modeltype}_prove_{nlayer}')), number=1)
 
     proof_size = os.path.getsize(proof_file)
     vk_size = os.path.getsize('vk.key')
@@ -79,15 +79,13 @@ def setup_and_prove(modeltype, nlayer):
     return modeltype, nlayer, params, macs, time_to_setup, time_to_prove, proof_size, vk_size, pk_size
 
 # Setup csv file
-FILENAME ='model_size_results_Sept5th.csv'
 with open(FILENAME, 'w') as f:
     f.write("modeltype,nlayer,param_count,macs,time_to_setup,time_to_prove,proof_size,vk_size,pk_size\n")
 
 results = [] # For local runtime experiments
-ranges = list(range(10, 30)) 
-# + list(range(20, 30, 2)) + list(range(30, 41, 5))
+ranges = list(range(1, 25)) 
 for nlayer in tqdm(ranges):
-    for modeltype in ['CNN', 'MLP']: #  'Attn',  'LSTM'
+    for modeltype in ['CNN', 'MLP', 'Attn', 'LSTM']:
         print("Running", modeltype, nlayer)
         try:
             result = setup_and_prove(modeltype, nlayer)
@@ -101,48 +99,3 @@ for nlayer in tqdm(ranges):
 print("Done with local experiments")
 print(results)
 
-
-
-# # This is just code to get the sizing right
-# def sizes(modeltype, nlayer):
-#     if modeltype == 'CNN':
-#         model = VariableCNN(nlayer)
-#         input_shape= [1,28,28]
-#         dummy_input = torch.randn(input_shape)
-#     elif modeltype == 'MLP':
-#         model = MLP(nlayer+int((nlayer**2)/2))
-#         input_shape= [1,256]
-#         dummy_input = torch.randn(input_shape)
-#     elif modeltype == 'Attn':
-#         model = SimpleTransformer(int(np.sqrt(nlayer)/2)+1, d_model=64+32*nlayer)
-#         input_shape= [1,16,64+32*nlayer]
-#         dummy_input = torch.randn(input_shape)
-#     elif modeltype == 'LSTM':
-#         temp_nlayer = int(np.sqrt(nlayer)/2)
-#         model = VariableLSTM(nlayer=temp_nlayer, input_size=8+8*nlayer, hidden_size=8+8*temp_nlayer)
-#         input_shape= [3+nlayer,8+8*nlayer]
-#         dummy_input = torch.randn(input_shape)
-#     else:
-#         raise ValueError("modeltype must be one of CNN, MLP, Attn, LSTM")
-    
-#     macs, params = profile(model, inputs=(dummy_input, ))
-#     return macs, params
-
-# size_results = []
-# ranges = list(range(0, 20)) + list(range(20, 30, 2)) + list(range(30, 41, 5))
-# for nlayer in tqdm(ranges):
-#     for modeltype in ['CNN', 'MLP', 'Attn', 'LSTM']:
-#         print("Running", modeltype, nlayer)
-#         try:
-#             macs, params = sizes(modeltype, nlayer)
-#             size_results.append([macs, params, nlayer, modeltype])
-#         except Exception as e:
-#             print("Failed with", e)
-
-
-# import seaborn as sns
-# import pandas as pd
-# size_results_df = pd.DataFrame(size_results, columns=['macs', 'params', 'nlayer', 'modeltype'])
-# sns.scatterplot(size_results_df, x='nlayer', y='macs', hue='modeltype')
-
-# size_results_df.sort_values(by='nlayer', ascending=False)
