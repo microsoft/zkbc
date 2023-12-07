1940240dfacb6e382e7b88c8f68b210fe20de70c
tobinsouth <tobin.south@gmail.com>
2023-12-07 16:18:39 +0000

    Fixing CelebA

--- src/CelebA_example.py
+++ src/CelebA_example.py
@@ -9,9 +9,10 @@ from torchvision import datasets, models, transforms
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 import ezkl
+import glob
 
 LOGGING = True
-os.makedirs('MNIST/logs', exist_ok=True)
+os.makedirs('CelebA/logs', exist_ok=True)
 pipstd = lambda fname: f" >> CelebA/logs/{fname}.log" if LOGGING else ""
 SRS_PATH = '../kzgs/kzg%d.srs'
 
@@ -32,18 +33,17 @@ dataset = datasets.CelebA('CelebA/data', download=False, transform=transform)
 train_dataset, test_dataset = torch.utils.data.random_split(dataset, [len(dataset)-1000, 1000])
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=1)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
 # %% 2. Make the basic model
 device = torch.device("cuda" if torch.cuda.is_available() else "mps" if  torch.backends.mps.is_available() else "cpu")
 
-
 # %% 2.1 Option 1: Custom sparse model for efficient proving
-from models.VariableCNN import SparseCNN
+# from models.VariableCNN import SparseCNN
 
-# Model initialization
-model = SparseCNN()
-model = model.to(device)
+# # Model initialization
+# model = SparseCNN()
+# model = model.to(device)
 
 
 # %% 2.2 Option 2: MobileNetV3
@@ -51,17 +51,17 @@ model = models.mobilenet_v3_small(pretrained=True)
 model.classifier[3] = nn.Linear(1024, 40)
 model = model.to(device)
 
-# Let's look at the size of the model
-from thop import profile
-example_input = next(iter(test_loader))[0][0].to("cpu")
-macs, params = profile(model, inputs=(example_input.unsqueeze(0), ))
-print(f"Total model params: {params}\nTotal model MACs (FLOPs): {macs}")
+# # Let's look at the size of the model
+# from thop import profile
+# example_input = next(iter(test_loader))[0][0].to("cpu")
+# macs, params = profile(model, inputs=(example_input.unsqueeze(0), ))
+# print(f"Total model params: {params}\nTotal model MACs (FLOPs): {macs}")
 
 # %% 3 Training
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.1)
 
-num_epochs = 3
+num_epochs = 1
 for epoch in range(num_epochs):
     model.train()
     total_loss = 0
@@ -80,10 +80,9 @@ for epoch in range(num_epochs):
         if i % 100 == 0:
              print(f"Epoch [{epoch+1}], Loss: {total_loss/100:.4f}")
              total_loss=0
-       
-
+        if i == 1000: break# The mobilenetv3 doesn't need much fine tuning at all.
+    
 # %% 4. Export the model and data for ezkl to use
-
 model.to("cpu")
 example_input = next(iter(test_loader))[0][0].to("cpu")  # Ensure input is 4D: [1, C, H, W]
 from utils.export import export
@@ -101,3 +100,31 @@ os.system("ezkl compile-circuit -M CelebA/network.onnx -S CelebA/settings.json -
 os.system("ezkl gen-witness -M CelebA/network.ezkl -D CelebA/input.json --output CelebA/witnessRandom.json" + pipstd('setup'))
 os.system("ezkl mock -M CelebA/network.ezkl --witness CelebA/witnessRandom.json" + pipstd('setup'))
 os.system(f"ezkl setup -M CelebA/network.ezkl --srs-path={SRS_PATH % logrows} --vk-path=CelebA/vk.key --pk-path=CelebA/pk.key" + pipstd('setup'))
+
+
+# %% 4.1 Witness generation
+for i, (image, label) in enumerate(tqdm(test_loader, desc="Exporting data")):
+    # remove batch dimension
+    output = model(image)
+    data_array = ((image).detach().numpy()).reshape([-1]).tolist()
+    data = dict(input_data = [data_array],
+                output_data = [((o).detach().numpy()).reshape([-1]).tolist() for o in output])
+
+    input_filename = f'CelebA/data/ezkl_inputs/input_{i}.json'
+    json.dump( data, open( input_filename, 'w' ) )
+
+# Now we generate witnesses
+for file in tqdm(glob.glob('CelebA/data/ezkl_inputs/input_*.json')):
+    filename = file.split('/')[-1]
+    res = ezkl.gen_witness(file, "CelebA/network.ezkl", f"CelebA/data/ezkl_witnesses/{filename}")
+
+# f =json.load(open('/u/tsouth/projects/zkbc/zkbc-msr/src/CelebA/data/ezkl_witnesses/input_344.json'))
+
+# import numpy as np
+# np.array(f['inputs']).shape
+
+# %% 4.2 Loop and prove
+for witness in tqdm(glob.glob('CelebA/data/ezkl_witnesses/*.json')):
+    filename = witness.split('/')[-1]
+    proof_path = f"CelebA/data/ezkl_proofs/{filename}.proof"
+    res = os.system(f"ezkl prove -M CelebA/network.ezkl --witness {witness} --pk-path=CelebA/pk.key --proof-path={proof_path} --srs-path={SRS_PATH % logrows}" + pipstd('prove'))
